5f5ceee576f32d2cd2b2d12c6daa2980b7491f00
jiahnkim230502 <locker9417@gmail.com>
2023-06-27 21:33:52 +0900

    불필요한 코드정리

--- routes/comments.route.js
+++ routes/comments.route.js
@@ -18,12 +18,6 @@ router.post("/posts/:postId/comments", authMiddleware, async (req, res) => {
     const post = await Posts.findOne({
         where: { postId },
     });
-    
-    if (post.postId !== postId) {
-        return res.status(403).json({
-            message: "게시글이 존재하지 않습니다.",
-        });
-    };
 
     if (comment.length === 0) {
         return res.status(403).json({
